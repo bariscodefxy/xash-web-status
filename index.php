d560163b0bbd28f2439e199deff5f507326fd15c
bariscodefx <bariscodefx@gmail.com>
2021-12-24 23:39:45 +0300

    fix jquery

--- index.php
+++ index.php
@@ -648,7 +648,7 @@ https://templatemo.com/tm-563-seo-dream
   </footer>
 
   <!-- Scripts -->
-  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/js/owl-carousel.js"></script>
   <script src="assets/js/animation.js"></script>
